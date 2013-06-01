c2857142d2b3b4635e897db6a7955108ef3b0f3c
Anand <anand_trex@yahoo.com>
2013-06-01 03:11:27 -0700

    Changing setup to copy directly instead of creating symlinks

--- setup.sh
+++ setup.sh
@@ -9,5 +9,5 @@ echo "Generating bootstrap js and css files"
 make
 cd ..
 echo "Creating symlinks to bootstrap js and css files"
-cp -rsf `pwd`/bootstrap/docs/assets/js/* `pwd`/content/media/js/libs
-cp -rsf `pwd`/bootstrap/docs/assets/css/* `pwd`/content/media/css
+cp -rf `pwd`/bootstrap/docs/assets/js/* `pwd`/content/media/js/libs
+cp -rf `pwd`/bootstrap/docs/assets/css/* `pwd`/content/media/css
