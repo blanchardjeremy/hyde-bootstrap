5a013dbf411cd4fe73b3c35addf118f7c141c235
Anand <anand_trex@yahoo.com>
2013-06-01 02:17:19 -0700

    Adding messages to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+echo "Fetching bootstrap from github"
 git submodule update
 cd bootstrap
+echo "Installing necessary bootstrap dependencies"
 npm install
+echo "Generating bootstrap js and css files"
 make
 cd ..
+echo "Creating symlinks to bootstrap js and css files"
 cp -rsf `pwd`/bootstrap/docs/assets/js/* `pwd`/content/media/js/libs
 cp -rsf `pwd`/bootstrap/docs/assets/css/* `pwd`/content/media/css
